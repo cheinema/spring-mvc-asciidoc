40cf9bf50b8e9439bb9da658885ce581c50ccb3f
Christian Heinemann <ch@chlab.net>
2019-01-10 15:00:35 +0100

    Bundle documentation in JAR and provide it under path '/'

--- src/main/java/net/chlab/sandbox/springmvcasciidoc/SpringMvcAsciiDocApplication.java
+++ src/main/java/net/chlab/sandbox/springmvcasciidoc/SpringMvcAsciiDocApplication.java
@@ -2,11 +2,23 @@ package net.chlab.sandbox.springmvcasciidoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringMvcAsciiDocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcAsciiDocApplication.class, args);
+    }
+
+    @Configuration
+    public static class WebAppConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addRedirectViewController("/", "docs/api-guide.html");
+        }
     }
 }
