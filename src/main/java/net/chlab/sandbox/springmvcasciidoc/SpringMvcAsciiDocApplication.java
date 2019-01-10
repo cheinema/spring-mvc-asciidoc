f1a94257cbc11a3efda0b6ec47c0e8ec2d6fbd14
Christian Heinemann <ch@chlab.net>
2019-01-10 12:51:28 +0100

    Create a new Spring Boot Application

---
+++ src/main/java/net/chlab/sandbox/springmvcasciidoc/SpringMvcAsciiDocApplication.java
@@ -0,0 +1,12 @@
+package net.chlab.sandbox.springmvcasciidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAsciiDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcAsciiDocApplication.class, args);
+    }
+}
---
+++ src/test/java/net/chlab/sandbox/springmvcasciidoc/SpringMvcAsciiDocApplicationTest.java
@@ -0,0 +1,15 @@
+package net.chlab.sandbox.springmvcasciidoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcAsciiDocApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
