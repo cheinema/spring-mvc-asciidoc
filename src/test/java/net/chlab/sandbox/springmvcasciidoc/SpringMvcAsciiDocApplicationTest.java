0341578317ae3fe5e1756bf07c78f194d7e063cb
Christian Heinemann <ch@chlab.net>
2019-01-10 13:14:50 +0100

    Switch to JUnit 5

--- src/test/java/net/chlab/sandbox/springmvcasciidoc/SpringMvcAsciiDocApplicationTest.java
+++ src/test/java/net/chlab/sandbox/springmvcasciidoc/SpringMvcAsciiDocApplicationTest.java
@@ -1,15 +1,12 @@
 package net.chlab.sandbox.springmvcasciidoc;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringMvcAsciiDocApplicationTest {
+class SpringMvcAsciiDocApplicationTest {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 }
